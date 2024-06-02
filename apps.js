6c261d84faefb4b2bba06658f27b9a526dcbc2db
SHIFA NAWED <156599970+SHIFANAWED1234@users.noreply.github.com>
2024-06-02 19:37:05 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,58 @@
+let score = 5;
+let q1= prompt('are you a human')
+let response1=q1
+if(response1===answer1){
+    alert('your answer is correct')
+    score++;
+}
+else{
+    alert('oops! wrong answer')
+}
+let q2 = promote ('are youn a develper ')
+let response2=q2
+if(response2===answer2){
+    alert('your answer is correct')
+    score++;
+}
+else{
+    alert('oops! wrong answer')
+}
+let q3 =prompt('what is your age ')
+let response3=q3
+if(response3===answer3){
+    alert('your answer is correct')
+    score++;
+}
+else{
+    alert('oops! wrong answer')
+}
+let q4 =prompt('are you a girl')
+let response4=q4
+if(response4===answer4){
+    alert('your answer is correct')
+    score++;
+}
+else{
+    alert('oops! wrong answer')
+}
+let q5 =promote('are you happy ')
+let response5=q5
+if(response5===answer5){
+    alert('your answer is correct')
+    score++;
+}
+else{
+    alert('oops! wrong answer')
+}
+
+
+if
+
+  
+let answer1='yes'
+let answer2='yes'
+let answer3='yes'
+let answer4='yes'
+let answer5='yes'
+
+
